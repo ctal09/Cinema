cadd7c62cb409798a5a8180fa207ce4338936a18
Sital <shitaldulal@gmail.com>
2025-04-02 09:29:53 +0545

    validation added

--- phase-1/src/Components/NavBar/footer.jsx
+++ phase-1/src/Components/NavBar/footer.jsx
@@ -3,11 +3,10 @@ function Footer() {
         <footer className="py-3 my-4">
             <ul className="nav justify-content-center border-bottom pb-3 mb-3">
                 <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Home</a></li>
-                <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Features</a></li>
-                <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Pricing</a></li>
+
                 <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">About</a></li>
             </ul>
-            <p className="text-center text-body-secondary">© 2024 Company, Inc</p>
+            <p className="text-center text-body-secondary">© 2025 </p>
         </footer>);
 };
 export default Footer;
